dbc9f9e3c67577eb2ad4e6382b127169b4f7609c
Dmytro Zozulia <z1dima1z@gmail.com>
2025-03-22 12:52:33 +0100

    chore: add autorization app

---
+++ autorization-service/test/autorization-service.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AutorizationService from '../lib/autorization-service-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/autorization-service-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AutorizationService.AutorizationServiceStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
